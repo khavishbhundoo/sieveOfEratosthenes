afda05f1f78d889c91f7a097e2dea09c657f74b3
Khavish Anshudass Bhundoo <khavishbhundoo@users.noreply.github.com>
2018-04-15 12:07:24 +0400

    Remove using namespace std;

--- SieveOfEratosthenes.cpp
+++ SieveOfEratosthenes.cpp
@@ -4,11 +4,11 @@
 #include <ctime>
 #include <fstream>
 
-using namespace std;
+
 using u_big = unsigned long long int;
 
-vector<u_big> sieveOfEratosthenes(u_big min, u_big max) {
-    vector<u_big> result;
+std::vector<u_big> sieveOfEratosthenes(u_big &min, u_big &max) {
+    std::vector<u_big> result;
 
     if (max <= 1 || min < 1 ) return result;
 
@@ -26,7 +26,7 @@ vector<u_big> sieveOfEratosthenes(u_big min, u_big max) {
      * Declare a vector of boolean and ser all value to true
      * Consider all numbers to be prime at this point
      */
-    vector<bool> primes(min + (max - min) + 1,true);
+    std::vector<bool> primes(min + (max - min) + 1,true);
 
     auto sqrt_max = (u_big) sqrt(max);
 
@@ -52,32 +52,31 @@ vector<u_big> sieveOfEratosthenes(u_big min, u_big max) {
 
 }
 
-void print(vector<u_big> result) {
+void print(std::vector<u_big> result) {
     for ( auto const &i : result ) {
-        cout << i << std::endl;
+        std::cout << i << std::endl;
     }
 }
 
-void save(const string &filename,vector<u_big> result) {
-    ofstream outFile(filename);
+void save(const std::string &filename,std::vector<u_big> &result) {
+    std::ofstream outFile(filename);
     for (const auto &p : result)  outFile << p << "\n";
 
 }
 
 int main() {
     unsigned long long int min,max;
-    cout << "Enter minimum : ";
-    cin >> min;
-    cout << "\nEnter maximum : ";
-    cin >> max;
+    std::cout << "Enter minimum : ";
+    std::cin >> min;
+    std::cout << "\nEnter maximum : ";
+    std::cin >> max;
     int start_s=clock();
-    vector<u_big> result = sieveOfEratosthenes(min,max);
+    std::vector<u_big> result = sieveOfEratosthenes(min,max);
     int stop_s=clock();
-    cout << "Runtime to generate primes : " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << " ms" << endl;
-    cout << "Number of primes : " <<  result.size() << endl;
-    cout << "Primes saved to primes.txt" << endl;
+    std::cout << "Runtime to generate primes : " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << " ms" << std::endl;
+    std::cout << "Number of primes : " <<  result.size() << std::endl;
+    std::cout << "Primes saved to primes.txt" << std::endl;
     save("primes.txt", result);
     //print(result);
     return 0;
 }
-
